aa72d9d58e12b2670fdca43426ea3905ef1da5a1
Sim Shang En <sim@shangen.org>
2024-07-29 23:13:18 +0800

    Update q3.py

--- PE2/PE 2 CS1010E 2022 23 Sem 1/q3.py
+++ PE2/PE 2 CS1010E 2022 23 Sem 1/q3.py
@@ -1,30 +1,29 @@
 def min_no_of_turns(L):
-    # strategy: (1) sort the array, (2) find longest consecutive subsequence, (3) eliminate the subsequence from the array, (4) repeat until array is empty
+    if not L:
+        return 0
 
-    L_copy = list(L)
-    L_copy.sort()
-    turns = 0
-    while L_copy:
-        print(L_copy)
-        # find the longest consecutive subsequence
-        longest_subsequence = []
-        current_subsequence = [L_copy[0]]
-        for i in range(1, len(L_copy)):
-            if L_copy[i] == L_copy[i - 1] + 1:
-                current_subsequence.append(L_copy[i])
-            else:
-                if len(current_subsequence) > len(longest_subsequence):
-                    longest_subsequence = current_subsequence
-                current_subsequence = [L_copy[i]]
-        if len(current_subsequence) > len(longest_subsequence):
-            longest_subsequence = current_subsequence
+    # Sort the list to handle consecutive sequences
+    sorted_L = sorted(L)
 
-        # remove the longest subsequence from the array
-        print(f"longest_subsequence: {longest_subsequence}")
-        for elem in longest_subsequence:
-            L_copy.remove(elem)
-        turns += 1
-    return turns
+    # Use a dictionary to group consecutive sequences
+    sequences = {}
+    current_seq = []
+    
+    for num in sorted_L:
+        if not current_seq or num == current_seq[-1] + 1:
+            current_seq.append(num)
+        else:
+            seq_key = tuple(current_seq)
+            sequences[seq_key] = len(current_seq)
+            current_seq = [num]
+    
+    # Add the last sequence
+    if current_seq:
+        seq_key = tuple(current_seq)
+        sequences[seq_key] = len(current_seq)
+
+    # The number of turns is the number of sequences
+    return len(sequences)
 
 
 # print(min_no_of_turns((1, 8, 3, 6, 5, 7, 2, 1)))
